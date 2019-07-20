9e5556978a852f9cf0e0de26944dd1d4ce142941
emilybaum <emily.r.baum@gmail.com>
2019-07-20 17:54:40 -0400

    converted the stings to numbers in the scores array

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// added this per jordan's demo
+app.use(express.static("public"));
+
 // requiring these here as if they are in this file
 require("./app/routing/htmlRoutes.js")(app);
 require("./app/routing/apiRoutes.js")(app);
