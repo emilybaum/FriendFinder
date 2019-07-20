51b259ca742969897cda2b03849e8295ecde22b3
emilybaum <emily.r.baum@gmail.com>
2019-07-20 00:50:24 -0400

    working on transitioning the string in the array to numbers

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,7 @@
 // Your apiRoutes.js file should contain two routes:
     // A GET route with the url / api / friends.This will be used to display a JSON of all possible friends.
     // A POST routes / api / friends.This will be used to handle incoming survey results.This route will also be used to handle the compatibility logic.
+
 var path = require("path");
 
 var friendsArray = require("../data/friends.js");
@@ -35,11 +36,8 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
         var newFiend = req.body;
-
         console.log(newFiend);
-
         friendsArray.push(newFiend);
-
         res.json(newFiend);
     });
 
