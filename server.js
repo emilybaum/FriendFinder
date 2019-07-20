a790434da09866afc644d076b3b1e7f0184793d2
emilybaum <emily.r.baum@gmail.com>
2019-07-20 09:58:59 -0400

    added the api link url link and got the post to flow properly

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+// requiring these here as if they are in this file
 require("./app/routing/htmlRoutes.js")(app);
 require("./app/routing/apiRoutes.js")(app);
 
