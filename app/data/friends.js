3e6a22896513d22337ec9434771480df1ac19821
emilybaum <emily.r.baum@gmail.com>
2019-07-22 01:07:26 -0400

    have captured the scoresArray so that I can compare to it later

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,50 @@
 var friendsArray = [
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Harry Potter",
+        "photo": "https://vignette.wikia.nocookie.net/harrypotter/images/8/8d/PromoHP7_Harry_Potter.jpg/revision/latest?cb=20140603201724",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Alexander Hamilton",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/0/05/Alexander_Hamilton_portrait_by_John_Trumbull_1806.jpg",
+        "scores": [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+        ]
+    },
+    {
+        "name": "Donald Duck",
+        "photo": "https://images.homedepot-static.com/productImages/d31251a7-fcd6-4614-8608-a27d38496abd/svn/white-roommates-wall-decals-rmk1512gm-64_600.jpg",
         "scores": [
             5,
-            1,
-            4,
-            4,
             5,
-            1,
-            2,
             5,
-            4,
-            1
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
         ]
     }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,6 @@
 // Your apiRoutes.js file should contain two routes:
-    // A GET route with the url / api / friends.This will be used to display a JSON of all possible friends.
-    // A POST routes / api / friends.This will be used to handle incoming survey results.This route will also be used to handle the compatibility logic.
+    // A GET route with the url/api/friends.This will be used to display a JSON of all possible friends.
+    // A POST routes /api/friends.This will be used to handle incoming survey results.This route will also be used to handle the compatibility logic.
 
 var friendsArray = require("../data/friends.js");
 
@@ -8,6 +8,7 @@ module.exports = function (app) {
 
     // ---------------------------------------------------------------------------
     app.get("/api/friends", function (req, res) {
+        // SHOWING ALL FRIENDS IN THE ARRAY
         return res.json(friendsArray);
     });
 
@@ -17,14 +18,183 @@ module.exports = function (app) {
     // API POST Requests
     app.post("/api/friends", function (req, res) {
         var surveyAnswers = req.body;
-        var scores = surveyAnswers.scores
+
+        console.log("friendsArray")
+        console.log(friendsArray);
+        console.log("-----------------------------")
 
         surveyAnswers.scores = surveyAnswers.scores.map(function (x) {
             return parseInt(x, 10);
         });
 
-        // friendsArray.push(surveyAnswers);
-        res.json(surveyAnswers);
+        var user = {
+            name: surveyAnswers.name,
+            photo: surveyAnswers.photo,
+            matchScore: function() {
+                var sum = 0;
+                for (var i = 0; i < surveyAnswers.scores.length; i++) {
+                    sum += surveyAnswers.scores[i];
+                };
+        
+                return sum;
+            }
+        }
+
+        
+
+
+        userMatchScore = user.matchScore();
+        console.log("Score for the current user");
+        console.log(userMatchScore);
+
+        
+        // var bestMatch = {
+        //     name: "",
+        //     photo: "",
+        //     matchScore: function (num, arr) {
+        //         var arr = friendsArray.scores;
+        //         var curr = arr[0];
+        //         var diff = Math.abs(num - curr);
+        //         for (var i = 0; i < arr.length; i++) {
+        //             var newdiff = Math.abs(num - arr[i])
+        //             if (newdiff < diff) {
+        //                 diff = newdiff;
+        //                 curr = arr[i];
+        //             }
+        //         }
+        //         return curr;
+        //     }
+        // }
+
+
+        var scoresArray = [];
+
+        bestMatch = {
+            name: "",
+            photo: "",
+            currentUser: user,
+            bestMatchDifference: 1000,
+        }
+
+
+        // get sum of the friendsArray[i].scores
+        // compare the sum to the user number
+        // update the best match with the detials from the friendsArray (the index will be the same for this)
+
+        getSumOfFriendScores(friendsArray)
+        function getSumOfFriendScores(arr) {
+
+            for (var i = 0; i < arr.length; i++) {
+                var scoresArray = arr[i].scores
+                pushSum(scoresArray)
+            }
+
+            closest(userMatchScore, scoresArray)
+        }
+
+        // function for finding the sum of an array?
+        function pushSum(arr) {
+            var sum = 0;
+            for (var i = 0; i < arr.length; i++) {
+                sum += arr[i];
+            };
+
+            scoresArray.push(sum);
+        }
+        
+        console.log("should is an array of the sum of friend scores")
+        console.log(scoresArray)
+        console.log("-------------------------")
+
+        
+        // function compareScores() {
+        //     var matchDifference = 0;
+        //     for (var i = 0; i < friendScores.length; i++) {
+        //         if (Math.abs(userMatchScore - friendScores[i]) < matchDifference) {
+        //             matchDifference = friendScores[i]
+        //         }
+        //         else {
+        //             console.log("index of: " + [i]);
+        //             return [i];
+        //         }
+        //     }
+        // }
+
+        function closest(num, arr) {
+            
+            console.log("arr in closest()");
+            console.log(arr);
+            console.log("------------------")
+
+            console.log("length in closest()");
+            console.log(arr.length);
+            console.log("------------------")
+
+
+            var curr = arr[0];
+            var index = []
+            var diff = Math.abs(num - curr);
+            for (var i = 0; i < arr.length; i++) {
+                var newdiff = Math.abs(num - arr[i]);
+                if (newdiff < diff) {
+                    diff = newdiff;
+                    curr = arr[i];
+                    index = [i];
+                }
+            }
+            console.log("this is hte index: " + index);
+            console.log(curr);
+            return index;
+        }
+
+        // for (var i = 0; i < friendsArray.length; i++) {
+        //     matchDifference = 0;
+        //     var friendsSum = 0;
+        //     console.log("level one");
+
+        //     for (var z = 0; z < friendsArray[i].scores.length; z++) {
+        //         friendsSum += friendsArray[i].scores[z];
+        //         console.log("level two");
+            
+        //     }
+
+        //     for (var x = 0; x < surveyAnswers.scores.length; x++) {
+        //         matchDifference += Math.abs(userMatchScore - friendsSum);
+        //         if (matchDifference < bestMatch.bestMatchDifference) {
+        //             bestMatch.name = this.name;
+        //             console.log(this.name);
+
+        //             bestMatch.photo = this.photo;
+        //             console.log(this.photo);
+
+        //             bestMatch.bestMatchDifference = matchDifference;
+        //             console.log(matchDifference);
+
+        //             console.log("level three");
+        //         }
+        //         else {
+        //             console.log("failed")
+        //         }
+
+        //     }
+            
+        // }
+
+        // console.log("result of the best match:")
+        // console.log(bestMatch)
+        // console.log("-----------------------------------")
+
+        // console.log("closest match?")
+        // console.log(bestMatch.matchScore(userMatchScore, friendsArray));
+
+        // function diff(a, b) { 
+        //     return Math.abs(a - b); 
+        // }
+
+
+        // SHOULD THIS ONLY USE THE NEW PERSON ADDED TO THE ARRAY??
+        friendsArray.push(surveyAnswers);
+        res.json(bestMatch);
     });
 
 };
