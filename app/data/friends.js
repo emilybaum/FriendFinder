590d640726ca0603194b8f368ec28536da88ceae
emilybaum <emily.r.baum@gmail.com>
2019-07-23 20:19:25 -0400

    added photos for characters

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,39 +1,151 @@
 var friendsArray = [
     {
-        "name": "Harry Potter",
-        "photo": "https://vignette.wikia.nocookie.net/harrypotter/images/8/8d/PromoHP7_Harry_Potter.jpg/revision/latest?cb=20140603201724",
+        "name": "Eleven",
+        "photo": "https://vignette.wikia.nocookie.net/strangerthings8338/images/f/f1/Eleven_S03_portrait.png/revision/latest/scale-to-width-down/286?cb=20190722075442",
+        "scores": [
+            3,
+            4,
+            5,
+            2,
+            1,
+            5,
+            5,
+            5,
+            1,
+            4
+        ]
+    },
+    {
+        "name": "Dustin",
+        "photo": "https://vignette.wikia.nocookie.net/strangerthings8338/images/1/18/Dustin_S2.png/revision/latest?cb=20180319174421",
         "scores": [
             5,
             2,
             4,
             1,
-            1,
-            4,
             3,
             1,
+            4,
             1,
-            3
+            2,
+            5,
         ]
     },
     {
-        "name": "Alexander Hamilton",
-        "photo": "https://upload.wikimedia.org/wikipedia/commons/0/05/Alexander_Hamilton_portrait_by_John_Trumbull_1806.jpg",
+        "name": "Mike",
+        "photo": "https://imgix.bustle.com/uploads/image/2017/10/30/f2814aa0-fe39-48fa-b8c2-5cf349099089-screen-shot-2017-10-29-at-93232-pm.png?w=970&h=546&fit=crop&crop=faces&auto=format&q=70",
+        "scores": [
+            3,
+            4,
+            5,
+            5,
+            2,
+            1,
+            2,
+            5,
+            5,
+            1,
+        ]
+    },
+    {
+        "name": "Will",
+        "photo": "https://pixel.nymag.com/imgs/daily/vulture/2017/10/27/27-will-byers-stranger-things.w700.h700.jpg",
+        "scores": [
+            1,
+            1,
+            5,
+            3,
+            3,
+            3,
+            2,
+            5,
+            3,
+            5,
+        ]
+    },
+    {
+        "name": "Lucas",
+        "photo": "https://assets.popbuzz.com/2017/45/lucas-stranger-things--1509974047-view-0.png",
+        "scores": [
+            2,
+            4,
+            1,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            3,
+        ]
+    },
+    {
+        "name": "Nancy",
+        "photo": "https://www.mordeo.org/files/uploads/2019/05/Nancy-In-Stranger-Things-3-4K-Ultra-HD-Mobile-Wallpaper-950x1689.jpg",
         "scores": [
             5,
+            3,
+            5,
+            1,
+            5,
+            1,
+            5,
+            1,
+            5,
+            2,
+        ]
+    },
+    {
+        "name": "Max",
+        "photo": "https://www.mordeo.org/files/uploads/2019/05/Max-In-Stranger-Things-3-4K-Ultra-HD-Mobile-Wallpaper-950x1689.jpg",
+        "scores": [
+            1,
+            3,
+            3,
+            3,
+            1,
+            4,
             3,
             4,
             2,
+            2,
+        ]
+    },
+    {
+        "name": "Chief Hopper",
+        "photo": "",
+        "scores": [
             1,
             5,
             5,
-            1,
-            1,
+            5,
+            3,
+            5,
+            5,
+            4,
+            4,
             1,
         ]
     },
     {
-        "name": "Donald Duck",
-        "photo": "https://images.homedepot-static.com/productImages/d31251a7-fcd6-4614-8608-a27d38496abd/svn/white-roommates-wall-decals-rmk1512gm-64_600.jpg",
+        "name": "Steve Harring",
+        "photo": "https://pixel.nymag.com/imgs/daily/vulture/2017/11/01/01-stranger-things-hairspray.w700.h700.jpg",
+        "scores": [
+            5,
+            3,
+            5,
+            2,
+            3,
+            4,
+            4,
+            2,
+            5,
+            4,
+        ]
+    },
+    {
+        "name": "Jonathan",
+        "photo": "https://assets.popbuzz.com/2017/44/jonathan-byers-stranger-things-1509370842-view-0.jpg",
         "scores": [
             5,
             5,
@@ -46,7 +158,7 @@ var friendsArray = [
             5,
             5,
         ]
-    }
+    },
 ];
 
 
@@ -59,23 +171,21 @@ module.exports = friendsArray;
 
 
 
-// EXAMPLE ----------------------------------------------------------------------
-// You should save your application's data inside of app/data/friends.js as an array of objects. Each of these objects should roughly follow the format below.
-    // {
-    //     "name": "Ahmed",
-    //         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    //             "scores": [
-    //                 5,
-    //                 1,
-    //                 4,
-    //                 4,
-    //                 5,
-    //                 1,
-    //                 2,
-    //                 5,
-    //                 4,
-    //                 1
-    //             ]
-    // }
-    // --------------------------------------------------------------------------
+// Eleven
+// Dustin
+// Mike
+// Will
+// Lucas
+// Nancy
+// Max
+// Chief Hopper
+// Steve Harring
+// Jonathan
+
+// Joyce
+// Billy
+// Robin - https://vignette.wikia.nocookie.net/strangerthings8338/images/f/fc/Robin_S3.png/revision/latest?cb=20190409051202
+// Karen
+// Bob Newby
+// Barbara
 
